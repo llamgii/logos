d626a5bcfb3735d931b3cf5ed33521460a927917
llamgii <63828310+llamgii@users.noreply.github.com>
2026-01-01 17:20:18 +0100

    Charge for tickets and announce destination

---
+++ 5M-Scripts/qbx_ped_interact/config.lua
@@ -0,0 +1,67 @@
+Config = {}
+
+Config.Peds = {
+    {
+        model = 'a_m_y_business_01',
+        coords = vec4(-1827.97, -1246.83, 13.45, 321.11),
+        scenario = 'WORLD_HUMAN_STAND_IMPATIENT',
+    },
+    {
+        model = 'a_m_y_business_01',
+        coords = vec4(232.1, -903.73, 30.69, 56.87),
+        scenario = 'WORLD_HUMAN_STAND_IMPATIENT',
+    },
+    {
+        model = 'a_m_y_business_01',
+        coords = vec4(-273.69, -1919.68, 29.95, 317.12),
+        scenario = 'WORLD_HUMAN_STAND_IMPATIENT',
+    },
+}
+
+Config.Target = {
+    label = 'Talk to Clerk',
+    icon = 'fa-solid fa-comment',
+    distance = 2.0,
+}
+
+Config.Reward = {
+    item = 'water',
+    count = 1,
+}
+
+Config.Ticket = {
+    item = 'streetfight_ticket',
+    count = 1,
+    price = 1000,
+}
+
+Config.TicketCurrencyItem = 'money'
+
+Config.PoliceJobName = 'police'
+
+Config.RefuseNotification = {
+    description = "We're sorry, we are not dealing with Police officers.",
+    type = 'error',
+}
+
+Config.AlreadyTicketNotification = {
+    description = 'You already took the ticket.',
+    type = 'error',
+}
+
+Config.InsufficientFundsNotification = {
+    description = 'You do not have enough money for the ticket.',
+    type = 'error',
+}
+
+Config.TicketAnnouncement = {
+    description = 'Ticket purchased. Go to Tequila-la Club with your ticket.',
+    type = 'inform',
+}
+
+Config.Menu = {
+    title = 'Corner Store Clerk',
+    description = 'Need something? I can give you a bottle of water.',
+    buttonLabel = 'Get Water',
+    ticketLabel = 'Get Streetfight Ticket',
+}
